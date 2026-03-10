dc0898e26920b8e447e8940f7ab40c3893b960ee
JohnKuerCamonias <camoniasjohnkuer@gmail.com>
2026-03-10 23:11:51 +0800

    sub

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+// Active Section in Navigation bar
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+};
 
 // Form Submit
 document.getElementById("contactForm").addEventListener("submit", function(e) {
